8b6d768a7ed9b7e02dbf8cf05290e0e822119fc7
Kun-Hsiang <utmostof9@gmail.com>
2019-03-10 18:04:53 +0800

    updates

--- models/trajGRU/trajGRU_run.py
+++ models/trajGRU/trajGRU_run.py
@@ -301,7 +301,7 @@ if __name__ == '__main__':
     for i in range(2,6):
         origin_wd = args.weight_decay
         if args.clip:
-            for j in [1]:
+            for j in [1,3,4,5]:
                 args.weight_decay = i*origin_wd
                 args.clip_max_norm = int(j*args.clip_max_norm)
                 args.result_folder = os.path.join(args.result_dir, 'BMSE_wd{:.4f}_cm{:02d}'.format(args.weight_decay, args.clip_max_norm))

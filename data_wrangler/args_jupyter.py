e816620249849590235ae7ff84f7fa2a3a995ac1
utmostof9 <utmostof9@gmail.com>
2019-03-22 17:16:29 +0800

    updates

--- data_wrangler/args_jupyter.py
+++ data_wrangler/args_jupyter.py
@@ -1,12 +1,17 @@
 # import argparse
+import os
+import math
 import easydict
 import numpy as np
 import pandas as pd
-import math
 import datetime as dt
-import os
 
-
+def checkpath(path):
+    if os.path.exists(path):
+        print('Path exists!')
+    else:
+        print('Not exists!')
+        
 def createfolder(directory):
     try:
         if not os.path.exists(directory):
@@ -32,36 +37,50 @@ def make_path(path, workfolder=None):
 
 args = easydict.EasyDict({
     'study_area': 'Taipei',
-    'radar_folder': os.path.expanduser('~/Onedrive/01_IIS/04_TY_research/01_Radar_data'),
-    'I_lat_l': 23.9125,
-    'I_lat_h': 26.15,
-    'I_lon_l': 120.4,
-    'I_lon_h': 122.6375,
-    'F_lat_l': 24.6625,
-    'F_lat_h': 25.4,
-    'F_lon_l': 121.15,
-    'F_lon_h': 121.8875,
-    'origin_lat_l': 20,
-    'origin_lat_h': 27,
-    'origin_lon_l': 118,
-    'origin_lon_h': 123.5,
-    'res_degree': 0.0125
+    'working_folder': os.path.expanduser('~/ssd/01_ty_research'),
+    'I_y': [23.9125, 26.15],
+    'I_x': [120.4, 122.6375],
+    'F_y': [24.6625, 25.4],
+    'F_x': [121.15, 121.8875],
+    'O_y': [20, 27],
+    'O_x': [118,123.5],
+    'res_degree': 0.0125,
+    'compression': 'bz2',
+    'figure_dpi': 150,
+    'RAD_cmap': ['#FFFFFF','#FFD8D8','#FFB8B8','#FF9090','#FF6060','#FF2020','#CC0000','#A00000'],
+    'RAD_level': [-5, 0, 10, 20, 30, 40, 50, 60, 70],
+    'QPE_cmap': ['#FFFFFF','#D2D2FF','#AAAAFF','#8282FF','#6A6AFF','#4242FF','#1A1AFF','#000090','#000040','#000030'],
+    'QPE_level': [-5, 0, 10, 20, 35, 50, 80, 120, 160, 200],
+    'QPF_cmap': ['#FFFFFF','#D2D2FF','#AAAAFF','#8282FF','#6A6AFF','#4242FF','#1A1AFF','#000090','#000040','#000030'],
+    'QPF_level': [-5, 0, 10, 20, 35, 50, 80, 120, 160, 200]
 })
 
-args.ty_list = make_path('ty_list.xlsx', args.radar_folder)
-args.sta_list = make_path('sta_list_all.xlsx', args.radar_folder)
-args.TW_map_file = make_path('TW_shapefile/gadm36_TWN_2', args.radar_folder)
-args.fortran_code_folder = 'fortran_codes/'
-args.original_files_folder = os.path.expanduser('~/ssd/research/origianal_radar_data_2012_2018')
-args.compressed_files_folder = make_path('01_compressed_files', args.radar_folder)
-args.files_folder = make_path('02_pandas_files', args.radar_folder)
-args.figures_folder = make_path('03_figures', args.radar_folder)
+args.ty_list = make_path('ty_list.xlsx', args.working_folder)
+args.sta_list = make_path('sta_list_all.xlsx', args.working_folder)
+args.TW_map_file = make_path(os.path.join('07_gis_data','03_TW_shapefile','gadm36_TWN_2'), args.working_folder)
+args.fortran_code_folder = make_path('08_fortran_codes', args.working_folder)
 
-args.I_shape = (math.ceil((args.I_lon_h-args.I_lon_l)/args.res_degree)+1,math.ceil((args.I_lat_h-args.I_lat_l)/args.res_degree)+1)
-args.F_shape = (math.ceil((args.F_lon_h-args.F_lon_l)/args.res_degree)+1,math.ceil((args.F_lat_h-args.F_lat_l)/args.res_degree)+1)
-args.origin_size = (math.ceil((args.origin_lat_h-args.origin_lat_l)/args.res_degree)+1, math.ceil((args.origin_lon_h-args.origin_lon_l)/args.res_degree)+1)
+# To set the path of the radar folders
+args.radar_folder = make_path('01_radar_data', args.working_folder)
+args.radar_original_data_folder = make_path(os.path.join('00_original_data', 'radar_data_2012_2018'), args.working_folder)
+args.radar_compressed_data_folder = make_path('01_compressed_files', args.radar_folder)
+args.radar_wrangled_data_folder = make_path('02_wrangled_files', args.radar_folder)
+args.radar_figures_folder = make_path('03_figures', args.radar_folder)
 
-args.I_x_left = int((args.I_lon_l-args.origin_lon_l)/args.res_degree + 1)
-args.I_x_right = int(args.I_x_left + (args.I_lon_h-args.I_lon_l)/args.res_degree + 1)
-args.I_y_low = int((args.I_lat_l-args.origin_lat_l)/args.res_degree + 1)
-args.I_y_high = int(args.I_y_low + (args.I_lat_h-args.I_lat_l)/args.res_degree + 1)
+# To set the path of meteorology data folders
+args.meteorology_folder = make_path('02_meteorological_data', args.working_folder)
+args.meteorology_wrangled_folder = make_path('01_wrangled_files', args.meteorology_folder)
+
+args.ty_info_folder = make_path('03_ty_info_data', args.working_folder)
+
+args.I_shape = (math.ceil((args.I_x[1]-args.I_x[0])/args.res_degree)+1, math.ceil((args.I_y[1]-args.I_y[0])/args.res_degree)+1)
+args.F_shape = (math.ceil((args.F_x[1]-args.F_x[0])/args.res_degree)+1, math.ceil((args.F_y[1]-args.F_y[0])/args.res_degree)+1)
+args.O_shape = (math.ceil((args.O_x[1]-args.O_x[0])/args.res_degree)+1, math.ceil((args.O_y[1]-args.O_y[0])/args.res_degree)+1)
+
+args.I_x_iloc = [int((args.I_x[0]-args.O_x[0])/args.res_degree), int((args.I_x[1]-args.O_x[0])/args.res_degree + 1)]
+args.I_y_iloc = [int((args.I_y[0]-args.O_y[0])/args.res_degree), int((args.I_y[1]-args.O_y[0])/args.res_degree + 1)]
+args.F_x_iloc = [int((args.F_x[0]-args.O_x[0])/args.res_degree), int((args.F_x[1]-args.O_x[0])/args.res_degree + 1)]
+args.F_y_iloc = [int((args.F_y[0]-args.O_y[0])/args.res_degree), int((args.F_y[1]-args.O_y[0])/args.res_degree + 1)]
+
+args.xaxis_list = np.around(np.linspace(args.I_x[0], args.I_x[1], args.I_shape[0]), decimals=4)
+args.yaxis_list = np.around(np.linspace(args.I_y[1], args.I_y[0], args.I_shape[1]), decimals=4)
---
+++ data_wrangler/meteorology_wrangler.py
@@ -0,0 +1,71 @@
+import re
+import numpy as np
+import pandas as pd
+
+
+def read_man(filename):
+    with open(filename, 'r') as f:
+        data_man = f.readlines()
+        
+    for idx, l in enumerate(data_man):
+        if l[:2] == '# ':
+            data_man[idx] = data_man[idx][2:]
+            break
+    columns = re.split('\s+', data_man[idx])
+    data_man = data_man[idx+1:]
+    data_man = pd.DataFrame([re.split('\s+', x) for _, x in enumerate(data_man)], columns=columns).set_index('stno')
+    data_man = data_man[['yyyymmddhh','PS01','TX01','RH01','WD01','WD02','PP01']].astype(float)
+    data_man['yyyymmddhh'] = (data_man['yyyymmddhh']-1).astype(int).astype(str)
+    return data_man
+    
+def read_auto(filename):
+    with open(filename, 'r') as f:
+        data_auto = f.readlines()
+        
+    for idx, l in enumerate(data_auto):
+        if l[:2] == '# ':
+            data_auto[idx] = data_auto[idx][2:]
+            break
+    columns = re.split('\s+', data_auto[idx])
+    data_auto = data_auto[idx+1:]
+    data_auto = pd.DataFrame([re.split('\s+', x) for _,x in enumerate(data_auto)], columns=columns).set_index('stno')
+    data_auto = data_auto[['yyyymmddhh','PS01','TX01','RH01','WD01','WD02','PP01']].astype(float)
+    data_auto['yyyymmddhh'] = (data_auto['yyyymmddhh']-1).astype(int).astype(str)
+    return data_auto
+
+def read_data(data_man, data_auto, sta_list):
+    data_man = read_man(filename=data_man)
+    data_auto = read_auto(filename=data_auto)
+    data = pd.concat([data_man, data_auto], axis=0, sort=False).reset_index()
+
+    data.fillna(-9999, inplace=True)
+    data.replace(-9996, np.nan, inplace=True)
+    data.fillna(method='bfill', inplace=True)
+    data.replace(-9998, 0.1, inplace=True)
+    data.replace(-9997, np.nan, inplace=True)
+    data.replace(-9999, np.nan, inplace=True)
+    data.replace(-9991, np.nan, inplace=True)
+    data.dropna(inplace=True)
+    
+    # data.set_index(['yyyymmddhh'], inplace=True)
+    data['yyyymmddhh'] = pd.to_datetime(data['yyyymmddhh'],format='%Y%m%d%H')
+    data.set_index(['stno','yyyymmddhh'], inplace=True)
+
+    for i in sta_list.index:
+        data.loc[i,'lat'] = sta_list.loc[i, 'Latitude']
+        data.loc[i,'lon'] = sta_list.loc[i, 'Longitude']
+        data.loc[i,'height'] = sta_list.loc[i, 'Height(m)']
+
+    data.reset_index(inplace=True)
+    data.set_index(['yyyymmddhh'], inplace=True)
+    data.dropna(inplace=True)
+    return data
+
+def localP_2_seaP(p: int or list, h, t):
+    if type(p) == list:
+        p = np.array(list)    
+    if type(h) == list:
+        h = np.array(list)
+    if type(t) == list:
+        t = np.array(list)
+    return p*10**(h/(18400*(1+t/273)))

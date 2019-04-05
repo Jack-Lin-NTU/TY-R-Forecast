0184c7506fbfffcf0a06d04dd9350103c5a0122c
utmostof9 <utmostof9@gmail.com>
2019-04-06 03:09:38 +0800

    update

---
+++ models/trajGRU_jupyter/train_no_weather_FP32.bash
@@ -0,0 +1,14 @@
+python train.py --able-cuda --lr-scheduler --clip --input-with-grid --input-with-QPE --normalize-target\
+                --gpu 0 --dtype float32 --lr 1e-3 --weight-decay 0.001
+                
+python train.py --able-cuda --lr-scheduler --clip --input-with-grid --input-with-QPE --normalize-target\
+                --gpu 0 --dtype float32 --lr 1e-3 --weight-decay 0.005
+                
+python train.py --able-cuda --lr-scheduler --clip --input-with-grid --input-with-QPE --normalize-target\
+                --gpu 0 --dtype float32 --lr 1e-3 --weight-decay 0.01
+                
+python train.py --able-cuda --lr-scheduler --clip --input-with-grid --input-with-QPE --normalize-target\
+                --gpu 0 --dtype float32 --lr 1e-3 --weight-decay 0.05
+                
+python train.py --able-cuda --lr-scheduler --clip --input-with-grid --input-with-QPE --normalize-target\
+                --gpu 0 --dtype float32 --lr 1e-3 --weight-decay 0.1

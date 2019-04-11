15defbb676d309d0fe2eb06d989a8910d45d53db
utmostof9 <utmostof9@gmail.com>
2019-04-12 02:43:58 +0800

    update

--- models/trajGRU_jupyter/train_weather.bash
+++ models/trajGRU_jupyter/train_weather.bash
@@ -4,8 +4,8 @@ python train.py --able-cuda --lr-scheduler --clip --input-with-grid --input-with
                 --weather-list WD01 --weather-list WD02 --weather-list TX01\
                 --gpu 1 --dtype float16 --lr 1e-3 --weight-decay 0.0001
                 
-python train.py --able-cuda --lr-scheduler --clip --input-with-grid --input-with-QPE --normalize-target\
+python train.py --able-cuda --clip --input-with-grid --input-with-QPE --normalize-target\
                 --I-x-l 121.3375 --I-x-h 121.7 --I-y-l 24.8125 --I-y-h 25.175\
                 --weather-list PP01 --weather-list PS01 --weather-list RH01\
                 --weather-list WD01 --weather-list WD02 --weather-list TX01\
-                --gpu 1 --dtype float16 --lr 1e-3 --weight-decay 0.0005
+                --gpu 1 --dtype float16 --lr 1e-3 --weight-decay 0.0001

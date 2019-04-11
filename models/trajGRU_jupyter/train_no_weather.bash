03785cae37b6d62f421e5f40a90581290adb4c71
utmostof9 <utmostof9@gmail.com>
2019-04-12 04:55:32 +0800

    updates

--- models/trajGRU_jupyter/train_no_weather.bash
+++ models/trajGRU_jupyter/train_no_weather.bash
@@ -1,6 +1,6 @@
 python train.py --able-cuda --lr-scheduler --clip --input-with-grid --input-with-QPE --normalize-target\
                 --I-x-l 121.3375 --I-x-h 121.7 --I-y-l 24.8125 --I-y-h 25.175\
-                --gpu 0 --lr 1e-3 --weight-decay 0.0001
+                --gpu 1 --lr 1e-3 --weight-decay 0.0001
                 
 # python train.py --able-cuda --lr-schedule --clip --input-with-grid --input-with-QPE --normalize-target\
 #                 --I-x-l 121.3375 --I-x-h 121.7 --I-y-l 24.8125 --I-y-h 25.175\
@@ -10,7 +10,7 @@ python train.py --able-cuda --lr-scheduler --clip --input-with-grid --input-with
 
 python train.py --able-cuda --clip --input-with-grid --input-with-QPE --normalize-target\
                 --I-x-l 121.3375 --I-x-h 121.7 --I-y-l 24.8125 --I-y-h 25.175\
-                --gpu 0 --lr 1e-3 --weight-decay 0.0001
+                --gpu 1 --lr 1e-3 --weight-decay 0.0001
 
 # python train.py --able-cuda --clip --input-with-grid --input-with-QPE --normalize-target\
 #                 --I-x-l 121.3375 --I-x-h 121.7 --I-y-l 24.8125 --I-y-h 25.175\

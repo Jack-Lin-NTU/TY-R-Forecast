16698e007f3db9fdbe4bcf509fdbbe6ea34e848a
utmostof9 <utmostof9@gamil.com>
2019-05-01 15:57:36 +0800

    updates

--- models/exp/run_trajGRU_same_warp.bash
+++ models/exp/run_trajGRU_same_warp.bash
@@ -1,3 +1,3 @@
-python run_trajGRU_same_warp.py --able-cuda --lr-scheduler --clip --input-with-grid --input-with-QPE --batch-size 4 --normalize-target --dtype float16 --gpu 0 --lr 0.0001 --weight-decay 0.00001 --batch-norm \
+python run_trajGRU_same_warp.py --able-cuda --lr-scheduler --clip --input-with-grid --input-with-QPE --batch-size 4 --normalize-target --value-dtype float16 --gpu 0 --lr 0.0001 --weight-decay 0.00001 --batch-norm \
 --I-x-l 118.9 --I-x-h 122.2875 --I-y-l 21.75 --I-y-h 25.1125 \
 --F-x-l 118.9 --F-x-h 122.2875 --F-y-l 21.75 --F-y-h 25.1125

a2a38106dd0dca7158ad1b93fb6e3a02c92148d4
utmostof9 <utmostof9@gmail.com>
2019-05-03 15:49:51 +0800

    updates

--- models/exp/run_trajGRU_ntarget.bash
+++ models/exp/run_trajGRU_ntarget.bash
@@ -1,5 +1,19 @@
 clear
 python run_trajGRU.py --able-cuda --lr-scheduler --clip --input-with-grid --input-with-QPE --normalize-target \
---gpu 0 --lr 0.001 --weight-decay 0.01 --batch-size 4 --train-num 10 --optimizer Adam --value-dtype float16 \
-# --I-x-l 118.9 --I-x-h 122.2675 --I-y-l 21.75 --I-y-h 25.1125 \
-# --F-x-l 118.9 --F-x-h 122.2675 --F-y-l 21.75 --F-y-h 25.1125
+--gpu 0 --lr 0.001 --weight-decay 0 --max-epochs 10 --batch-size 4 --train-num 10 --optimizer Adam --value-dtype float16 \
+
+clear
+python run_trajGRU.py --able-cuda --lr-scheduler --clip --input-with-grid --input-with-QPE --normalize-target \
+--gpu 0 --lr 0.001 --weight-decay 0.1 --max-epochs 10 --batch-size 4 --train-num 10 --optimizer Adam --value-dtype float16 \
+
+clear
+python run_trajGRU.py --able-cuda --lr-scheduler --clip --input-with-grid --input-with-QPE --normalize-target \
+--gpu 0 --lr 0.001 --weight-decay 0.01 --max-epochs 10 --batch-size 4 --train-num 10 --optimizer Adam --value-dtype float16 \
+
+clear
+python run_trajGRU.py --able-cuda --lr-scheduler --clip --input-with-grid --input-with-QPE --normalize-target \
+--gpu 0 --lr 0.001 --weight-decay 0.001 --max-epochs 10 --batch-size 4 --train-num 10 --optimizer Adam --value-dtype float16 \
+
+clear
+python run_trajGRU.py --able-cuda --lr-scheduler --clip --input-with-grid --input-with-QPE --normalize-target \
+--gpu 0 --lr 0.001 --weight-decay 0.0001 --max-epochs 10 --batch-size 4 --train-num 10 --optimizer Adam --value-dtype float16 \

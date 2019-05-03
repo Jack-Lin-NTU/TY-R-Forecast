fbe618a491de12d0ed7dfac291461205d5b05705
utmostof9 <utmostof9@gmail.com>
2019-05-03 14:17:16 +0800

    updates

--- models/trajGRU/server_I.10_F.18_grid_weight-control.bash
+++
@@ -1,9 +0,0 @@
-python trajGRU_run.py \
-    --input-frames 10   \
-    --root-dir 01_Radar_data/02_numpy_files \
-    --ty-list-file ty_list.xlsx \
-    --result-dir 04_results/server \
-	--params-dir 05_params/server	\
-    --I-lat-l 24.6625 --I-lat-h 25.4 --I-lon-l 121.15 --I-lon-h 121.8875 \
-    --F-lat-l 24.6625 --F-lat-h 25.4 --F-lon-l 121.15 --F-lon-h 121.8875 \
-    --gpu 1 --input-with-grid --clip --lr-scheduler

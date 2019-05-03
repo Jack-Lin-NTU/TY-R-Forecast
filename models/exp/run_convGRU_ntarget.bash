6ca1f466340df99fe769a4cfd18231317a0cee51
utmostof9 <utmostof9@gmail.com>
2019-05-03 14:28:29 +0800

    updates

--- models/exp/run_convGRU_ntarget.bash
+++ models/exp/run_convGRU_ntarget.bash
@@ -1,5 +1,5 @@
 clear
 python run_convGRU.py --able-cuda --lr-scheduler --clip --input-with-grid --input-with-QPE --normalize-target \
---gpu 1 --lr 0.001 --weight-decay 0.1 --batch-size 8 --optimizer Adam --value-dtype float16 \
+--gpu 1 --lr 0.001 --weight-decay 0.1 --batch-size 12 --train-num 10 --optimizer Adam --value-dtype float16 \
 --I-x-l 118.9 --I-x-h 122.2675 --I-y-l 21.75 --I-y-h 25.1125 \
 --F-x-l 118.9 --F-x-h 122.2675 --F-y-l 21.75 --F-y-h 25.1125

ae0a7dbc9c0ef6b793298d3bb1468aac6d293956
Kun-Hsiang <utmostof9@gmail.com>
2019-05-04 18:56:04 +0800

    updates

--- models/exp/src/argstools/argstools.py
+++ models/exp/src/argstools/argstools.py
@@ -91,8 +91,6 @@ class BMSE(nn.Module):
         
     def forward(self, outputs, targets):
         loss = 0.
-        outputs = outputs.to(dtype=torch.float64)
-        targets = targets.to(dtype=torch.float64)
         for i in range(len(self.value_list)-1):
             mask = torch.cat([(targets>=self.value_list[i]).unsqueeze(2), (targets<self.value_list[i+1]).unsqueeze(2)], dim=2).all(dim=2)
             tmp = self.weights[i] * F.mse_loss(outputs[mask], targets[mask])

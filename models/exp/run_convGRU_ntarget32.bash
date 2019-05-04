1ce1e33c057e1c51eebc5ec34b6ae37be0fa2cb5
Kun-Hsiang <utmostof9@gmail.com>
2019-05-04 18:56:49 +0800

    updates

--- models/exp/run_convGRU_ntarget32.bash
+++ models/exp/run_convGRU_ntarget32.bash
@@ -1,6 +1,6 @@
 clear
 python run_convGRU.py --able-cuda --lr-scheduler --clip --input-with-grid --input-with-QPE --normalize-target \
---gpu 0 --lr 0.001 --weight-decay 0 --max-epochs 10 --batch-size 8 --train-num 10 --optimizer Adam --value-dtype float32 \
+--gpu 0 --lr 0.001 --weight-decay 0 --max-epochs 10 --batch-size 4 --train-num 10 --optimizer Adam --value-dtype float32 \
 
 
 # --I-x-l 118.9 --I-x-h 122.2675 --I-y-l 21.75 --I-y-h 25.1125 \

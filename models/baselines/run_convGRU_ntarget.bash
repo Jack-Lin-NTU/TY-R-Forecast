0fdc4044e5929bb8563e5aaf007f826bc7a82920
utmostof9 <utmostof9@gmail.com>
2019-05-06 15:37:39 +0800

    updates

--- models/baselines/run_convGRU_ntarget.bash
+++ models/baselines/run_convGRU_ntarget.bash
@@ -1,3 +1,3 @@
 clear
-python train_GRUs.py --model CONVGRU --able-cuda --lr-scheduler --clip --clip-max-norm 0.5 --input-with-grid --input-with-QPE --normalize-target \
+python train_GRUs.py --model CONVGRU --able-cuda --lr-scheduler --clip --clip-max-norm 0.09 --input-with-grid --input-with-QPE --normalize-target \
 --gpu 0 --lr 0.0005 --weight-decay 0 --max-epochs 50 --batch-size 8 --train-num 10 --optimizer Adam --value-dtype float16 \
--- models/baselines/run_trajGRU_ntarget.bash
+++ models/baselines/run_trajGRU_ntarget.bash
@@ -1,3 +1,12 @@
 clear
-python train_GRUs.py --model TRAJGRU --able-cuda --lr-scheduler --clip --clip-max-norm 0.4 --input-with-grid --input-with-QPE --normalize-target \
---gpu 0 --lr 0.0005 --weight-decay 0 --max-epochs 50 --batch-size 4 --train-num 10 --optimizer Adam --value-dtype float16 \
+python train_GRUs.py --model TRAJGRU --able-cuda --lr-scheduler --clip --clip-max-norm 0.09 --input-with-grid --input-with-QPE --normalize-target \
+--gpu 0 --lr 0.0005 --weight-decay 0 --max-epochs 10 --batch-size 4 --train-num 10 --optimizer Adam --value-dtype float16 \
+
+python train_GRUs.py --model TRAJGRU --able-cuda --lr-scheduler --clip --clip-max-norm 0.08 --input-with-grid --input-with-QPE --normalize-target \
+--gpu 0 --lr 0.0005 --weight-decay 0 --max-epochs 10 --batch-size 4 --train-num 10 --optimizer Adam --value-dtype float16 \
+
+python train_GRUs.py --model TRAJGRU --able-cuda --lr-scheduler --clip --clip-max-norm 0.07 --input-with-grid --input-with-QPE --normalize-target \
+--gpu 0 --lr 0.0005 --weight-decay 0 --max-epochs 10 --batch-size 4 --train-num 10 --optimizer Adam --value-dtype float16 \
+
+python train_GRUs.py --model TRAJGRU --able-cuda --lr-scheduler --clip --clip-max-norm 0.06 --input-with-grid --input-with-QPE --normalize-target \
+--gpu 0 --lr 0.0005 --weight-decay 0 --max-epochs 10 --batch-size 4 --train-num 10 --optimizer Adam --value-dtype float16 \

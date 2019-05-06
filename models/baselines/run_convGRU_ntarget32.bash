09e480515f7b975654ecc9fc1710e697762522dc
utmostof9 <utmostof9@gmail.com>
2019-05-06 15:13:04 +0800

    updates

--- models/baselines/run_convGRU_ntarget32.bash
+++ models/baselines/run_convGRU_ntarget32.bash
@@ -1,5 +1,6 @@
 clear
-python train_GRUs.py --model CONVGRU --able-cuda --lr-scheduler --clip --clip-max-norm 0.5 timizer Adam --value-dtype float32 \
+python train_GRUs.py --model CONVGRU --able-cuda --lr-scheduler --clip --clip-max-norm 0.5 --input-with-grid --input-with-QPE --normalize-target \
+--gpu 0 --lr 0.0005 --weight-decay 0 --max-epochs 50 --batch-size 8 --train-num 10 --optimizer Adam --value-dtype float32 \
 
 # --I-x-l 118.9 --I-x-h 122.2675 --I-y-l 21.75 --I-y-h 25.1125 \
 # --F-x-l 118.9 --F-x-h 122.2675 --F-y-l 21.75 --F-y-h 25.1125
--- models/baselines/run_trajGRU_ntarget.bash
+++ models/baselines/run_trajGRU_ntarget.bash
@@ -1,3 +1,3 @@
 clear
-python train_GRUs.py --model TRAJGRU --able-cuda --lr-scheduler --clip --clip-max-norm 0.5 --input-with-grid --input-with-QPE --normalize-target \
+python train_GRUs.py --model TRAJGRU --able-cuda --lr-scheduler --clip --clip-max-norm 0.4 --input-with-grid --input-with-QPE --normalize-target \
 --gpu 0 --lr 0.0005 --weight-decay 0 --max-epochs 50 --batch-size 4 --train-num 10 --optimizer Adam --value-dtype float16 \

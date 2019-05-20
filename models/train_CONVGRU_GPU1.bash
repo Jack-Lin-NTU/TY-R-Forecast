5f3e693c667f8614a1b8a8cfc790c21585630b72
utmostof9 <utmostof9@gmail.com>
2019-05-20 11:41:31 +0800

    add bash file

---
+++ models/train_CONVGRU_GPU1.bash
@@ -0,0 +1,3 @@
+clear
+python train_GRUs.py --model CONVGRU --able-cuda --lr-scheduler --clip --clip-max-norm 5  --input-with-grid \
+--gpu 1 --lr 0.0005 --weight-decay 0 --max-epochs 50 --batch-size 6 --train-num 10 --optimizer Adam --value-dtype float32 \

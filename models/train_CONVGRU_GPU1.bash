5eda6f6ed098756110ba42fde120fdc147b7503e
utmostof9 <utmostof9@gmail.com>
2019-05-21 11:19:13 +0800

    updates

--- models/train_CONVGRU_GPU1.bash
+++ models/train_CONVGRU_GPU1.bash
@@ -4,9 +4,9 @@
 # --gpu 1 --lr 0.0005 --weight-decay 0 --max-epochs 100 --batch-size 6 --train-num 10 --optimizer Adam --value-dtype float32 \
 
 # RAD_no_weather_scheduler_Adam(X)
-clear
-python train_GRUs.py --model CONVGRU --able-cuda --lr-scheduler --clip --clip-max-norm 5 \
---gpu 1 --lr 0.0005 --weight-decay 0 --max-epochs 100 --batch-size 6 --train-num 10 --optimizer Adam --value-dtype float32 \
+# clear
+# python train_GRUs.py --model CONVGRU --able-cuda --lr-scheduler --clip --clip-max-norm 5 \
+# --gpu 1 --lr 0.0005 --weight-decay 0 --max-epochs 100 --batch-size 6 --train-num 10 --optimizer Adam --value-dtype float32 \
 
 # RAD_no_weather_grid_scheduler_Adam
 clear

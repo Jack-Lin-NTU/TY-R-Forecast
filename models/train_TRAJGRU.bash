babedb757142dac53536b25bf5c431551c5fce0a
utmostof9 <utmostof9@gmail.com>
2019-05-27 17:21:47 +0800

    updates

--- models/train_TRAJGRU.bash
+++ models/train_TRAJGRU.bash
@@ -1,3 +1,3 @@
 clear
-python train_GRUs.py --model TRAJGRU --able-cuda --lr-scheduler --clip --clip-max-norm 5 --input-with-grid \
+python train_GRUs.py --model TRAJGRU --able-cuda --lr-scheduler --clip --clip-max-norm 5 --parallel-compute --input-with-grid \
 --gpu 0 --lr 0.0003 --weight-decay 0 --max-epochs 100 --batch-size 4 --train-num 10 --optimizer Adam --value-dtype float32 \

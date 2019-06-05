83720455616109e5d21b453ac30bec9d968e3d98
utmostof9 <utmostof9@gmail.com>
2019-06-05 18:30:26 +0800

    updates

--- models/train_TRAJGRU.2.bash
+++ models/train_TRAJGRU.2.bash
@@ -1,3 +1,3 @@
 clear
 python train_GRUs.py --model TRAJGRU --able-cuda --lr-scheduler --input-with-grid \
---gpu 2 --lr 0.0001 --weight-decay 0 --max-epochs 100 --batch-size 3 --train-num 10 --optimizer Adam --value-dtype float32 \
+--gpu 2 --lr 0.00008 --weight-decay 0 --max-epochs 100 --batch-size 3 --train-num 10 --optimizer Adam --value-dtype float32 \

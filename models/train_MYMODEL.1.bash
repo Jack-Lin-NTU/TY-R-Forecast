b257eb925b306e4055a9f36e2fd2be97c61575b6
utmostof9 <utmostof9@gmail.com>
2019-06-17 16:04:39 +0800

    updates

--- models/train_MYMODEL.1.bash
+++ models/train_MYMODEL.1.bash
@@ -1,3 +1,3 @@
 clear
 python train_my_model.py --model MYMULTIMODEL --able-cuda --lr-scheduler --clip --clip-max-norm 5 \
---gpu 1 --lr 0.001 --weight-decay 0 --max-epochs 50 --batch-size 4 --train-num 10 --optimizer Adam --value-dtype float32 \
+--gpu 1 --lr 0.0001 --weight-decay 0 --max-epochs 200 --batch-size 5 --train-num 10 --optimizer Adam --value-dtype float32 \

405b73362c38a9a645234de6240dcdf2a07ce646
utmostof9 <utmostof9@gmail.com>
2019-06-17 16:04:01 +0800

    updates

--- models/train_CONVGRU.0.bash
+++ models/train_CONVGRU.0.bash
@@ -1,3 +1,3 @@
 clear
 python train_GRUs.py --model CONVGRU --able-cuda --lr-scheduler --clip --clip-max-norm 5 --input-with-grid \
---gpu 0 --lr 0.0001 --weight-decay 0 --max-epochs 100 --batch-size 5 --train-num 10 --optimizer Adam --value-dtype float32
+--gpu 0 --lr 0.0001 --weight-decay 0 --max-epochs 100 --batch-size 6 --train-num 10 --optimizer Adam --value-dtype float32
--- models/train_MYMODEL.0.bash
+++ models/train_MYMODEL.0.bash
@@ -1,3 +1,3 @@
 clear
 python train_GRUs.py --model MYMODEL --able-cuda --lr-scheduler --clip --clip-max-norm 5 \
---gpu 0 --lr 0.001 --weight-decay 0 --max-epochs 100 --batch-size 4 --train-num 10 --optimizer Adam --value-dtype float32 \
+--gpu 0 --lr 0.0001 --weight-decay 0 --max-epochs 100 --batch-size 5 --train-num 10 --optimizer Adam --value-dtype float32 \

cb0f9b51a312ace11d20e9f67a3b9cc42693c4e7
utmostof9 <utmostof9@gmail.com>
2019-06-26 09:54:41 +0800

    updates

--- models/infer_TRAJGRU.bash
+++ models/infer_TRAJGRU.bash
@@ -1,4 +1,4 @@
 clear
 python infer_GRUs.py --model TRAJGRU --multi-unit --able-cuda --target-RAD --normalize-input \
---gpu 0 --lr 0.0001 --lr-scheduler --clip --clip-max-norm 0.001 --weight-decay 0 \
+--gpu 0 --lr 0.0001 --lr-scheduler --clip --clip-max-norm 5 --weight-decay 0 \
 --max-epochs 100 --batch-size 1 --train-num 10 --optimizer Adam --value-dtype float32

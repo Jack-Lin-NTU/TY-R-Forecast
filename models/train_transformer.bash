d82a9cf75715feb610fe967a480d75fb52eddf44
Kun-Hsiang, Lin <utmostof9@gmail.com>
2019-09-18 01:35:12 +0800

    updates

--- models/train_transformer.bash
+++ models/train_transformer.bash
@@ -1,4 +1,4 @@
 clear
 python train_transformer.py --model TRANSFORMER --able-cuda \
 --gpu 0 --lr 0.0001 --weight-decay 0.1 --I-size 150 --F-size 150 \
---max-epochs 30 --batch-size 4 --train-num 10 --optimizer Adam --value-dtype float16
+--max-epochs 30 --batch-size 6 --train-num 10 --optimizer Adam --value-dtype float16

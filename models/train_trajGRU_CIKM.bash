5924b6849e7f6ba2bc4990ccf8d12d35d31e077e
utmostof9 <utmostof9@gmail.com>
2019-10-01 08:31:33 +0800

    updates

--- models/train_trajGRU_CIKM.bash
+++ models/train_trajGRU_CIKM.bash
@@ -1,5 +1,5 @@
 clear
 python train_trajGRU_CIKM.py --model TRAJGRU --able-cuda --dataset CIKM \
 --gpu 0 --lr 0.0001 --weight-decay 0.1 --I-size 101 --F-size 101 \
---max-epochs 30 --batch-size 10 --train-num 10 --optimizer Adam --value-dtype float32 \
+--max-epochs 30 --batch-size 16 --train-num 10 --optimizer Adam --value-dtype float32 \
 --I-nframes 5 --F-nframes 10
